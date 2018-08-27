fa88656a340a4b6a2f499d77ce37b818e3ecd1f3
East Liu <east.liu@chinanetcloud.com>
2018-08-27 02:47:00 +0000

    Update opsstack-install.sh for no quotation mark

--- src/opsstack-install.sh
+++ src/opsstack-install.sh
@@ -103,7 +103,7 @@ elif [[ -f '/etc/system-release' ]] && [[ `cat /etc/system-release` == Amazon* ]
     # 2015 is last or else will error on cat redhat-release
     elif [[ `cat /etc/system-release | awk '{print $5}'` == 2018* ]] ; then
         OSVER="2018"
-    elif [[ `cat /etc/system-release ` == "Amazon Linux 2" ]] ; then"
+    elif [[ `cat /etc/system-release ` == "Amazon Linux 2" ]] ; then
         OSVER="2"
     else
         msg_err
